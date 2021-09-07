273663808682be9875ac5042e2ad6b5578e8959c
Siddiqui-Shahid <71084660+Siddiqui-Shahid@users.noreply.github.com>
2021-09-07 12:47:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    json_file = {}
+    json_file['query'] = 'hello_world'
+    return jsonify(json_file)
 
 
 if __name__ == '__main__':
